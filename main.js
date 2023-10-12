07ab3119d9573c950acc5b4bdc5dc3427ab4f5cb
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-12 20:45:11 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let a = 100
+let b = 200
+let c = 300
+
+let zmienna = [a, b, c]
+console.log(zmienna)
+
+
+
+/* Opis
+
+1. Stwórz plik main.js
+2. Zdeklaruj trzy zmienne o dowolnych nazwach - do każdej z nich przypisz dowolną liczbę.
+3. Stwórz zmienną o dowolnej nazwie, przypisz do niej tablicę, której elementami będą wcześniej zdeklarowane zmienne.
+
+Dodaj plik do repozytorium zdalnego i podeślij link do sprawdzenia.
+ */
